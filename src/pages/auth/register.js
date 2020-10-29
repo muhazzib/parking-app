d4e9f7e50fcc09831c7d97f1518fa61362dd326f
muhazzib <muhazzib7@gmail.com>
2020-10-30 04:53:49 +0500

    changed helper text

--- src/pages/auth/register.js
+++ src/pages/auth/register.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
 import DefaultButton from '../../components/button';
 import DefaultFormGroup from '../../components/form-group';
@@ -54,7 +54,7 @@ const Register = () => {
             <DefaultFormGroup onChange={getFormValues} name='username' value={user.username} required={true} type='text' label='Username' placeholder='Enter username' controlId='formBasicUsername' />
             <DefaultFormGroup onChange={getFormValues} name='email' value={user.email} required={true} type='email' label='Email Address' placeholder='Enter email' controlId='formBasicEmail' />
             <DefaultFormGroup onChange={getFormValues} name='password' value={user.password} required={true} type='password' label='Password' placeholder='Password' controlId='formBasicPassword' />
-            <Link to='login' className='d-block'>Already a registered User ?</Link>
+            <Link to='login' className='d-block'>Already have an Account ?</Link>
             <DefaultButton loading={loading} className='float-right' type='submit' title='Register' onClick={() => {
 
             }} />
